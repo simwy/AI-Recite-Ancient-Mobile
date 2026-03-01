be0a15fea98553f51625e89a1a1f2f0c32cca257
chenshiming0802 <chenshiming0802@163.com>
2026-03-01 09:37:13 +0800

    清理git

--- unpackage/dist/dev/mp-weixin/common/assets.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const _imports_0$2 = "/assets/face-verify-icon.da277dd7.svg";
-const _imports_0$1 = "/uni_modules/uni-sign-in/static/background.png";
-const _imports_0 = "/static/uni-load-state/disconnection.png";
-exports._imports_0 = _imports_0$2;
-exports._imports_0$1 = _imports_0$1;
-exports._imports_0$2 = _imports_0;
-//# sourceMappingURL=../../.sourcemap/mp-weixin/common/assets.js.map
